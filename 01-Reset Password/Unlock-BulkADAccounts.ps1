9325e5611fc0938d3c25e2b6ad5449c660c49698
Lutero-IT <lutero.it@gmail.com>
2026-06-08 12:59:08 +0200

    refactor: Finish indentation deployment and introduce wrapper functions
    There were places in the script where indentation was not implemented,
    so code review was conducted in which visual flaws were found, and layout
    inconsistencies resolved.
    
    Introduced custom wrapper functions which modified the original behavior
    of Write-Host and Read-Host cmdlets by adding a tab and a newline after
    each message, successfully completing the indentation standardization.

--- 01-Reset Password/Unlock-BulkADAccounts.ps1
+++ 01-Reset Password/Unlock-BulkADAccounts.ps1
@@ -4,33 +4,51 @@
 # 4. Unlock only specifed users
 
 $Indent = "`t"
-# add indent and make atomic commit NOW!!!
+# add function to combine indentation and newline and make ATOMIC COMMIT!
+
+# function created to replace classical Write-IndentedLog to add indentation on the
+# beginning of each message and newline below it
+function Write-IndentedLog ($Message, $Color = "White") {
+    Write-Host "${Indent}$Message`n" -ForegroundColor $Color
+}
+
+function Read-IndentedLog ($Message, $Color = "White") {
+    Read-Host "${Indent}$Message"
+}
+
+# MAKE ATOMIC COMMIT AFTER IMPLEMENTING READ-INDETNEDLOG FUNCIOTN AND
+# CODE REFACTORING !!!
+
+# COMMITS PLAN:
+# 0 commit - after Read-IndentedLog function
+# 1 commit - after finishing indentation
+# 2 commit - after refactoring sricpt and implementing colors to output messages
 
 # POINT 1 & 2 #
-Write-Host "${Indent}"
-Write-Host "${Indent}Checking if there are any locked accounts..."
+Write-IndentedLog ""
+Write-IndentedLog "Checking if there are any locked accounts..."
 $result = Search-ADAccount -LockedOut
 $formattedResult = $result | Format-Table Name, ObjectClass, LockedOut | Out-String
 $indentedResult = $formattedResult -replace '(?m)^', "$Indent"
 # tu dodać ograniczenie do wyświetlanych users
 
 if ($result -eq $null) {
-    Write-Host "${Indent}No locked accounts found in the database!"
+    Write-IndentedLog "No locked accounts found in the database!"
     } else {
-        Write-Host "${Indent}Found locked accounts in the database!"
+        Write-IndentedLog "Found locked accounts in the database!"
         $indentedResult  # ogranicz liczbe wyswietlanych users do 50 lub 100 dla wielkich korporacji
-        # Write-Host "${Indent}" z $formattedResult nie działa, zobaczyć dlaczego
+        # Write-IndentedLog "" z $formattedResult nie działa, zobaczyć dlaczego
 
         ### LOOP ###
 
         while ($result.Count -gt 0) {
-            Write-Host "${Indent}Which accounts do you wish to unlock?"
-            Write-Host "${Indent}OPTIONS:" -ForegroundColor Cyan
+            Write-IndentedLog "Which accounts do you wish to unlock?"
+            Write-IndentedLog "OPTIONS:" -ForegroundColor Yellow # ForeGround nie działa, dlaczego?
             Write-Host "${Indent}1. User/Users" -ForegroundColor Yellow
             Write-Host "${Indent}2. All" -ForegroundColor Yellow
             Write-Host "${Indent}3. Exit" -ForegroundColor Yellow
-            Write-Host "${Indent}4. Show list" -ForegroundColor Yellow
-            $userChoice = Read-Host "${Indent}Type option number or option value"
+            Write-Host "${Indent}4. Show list`n" -ForegroundColor Yellow
+            $userChoice = Read-IndentedLog "Type option number or option value"
 
             # Options Lits #
             $userOptions = "1","one","user","users"
@@ -40,66 +58,70 @@ if ($result -eq $null) {
 
             # OPTION 1: Unlock User/Users
             if ($userChoice -in $userOptions) {
-                Write-Host "${Indent}Chose option number 1 - Unlock User or Users"
-                Write-Host "${Indent}Type user or list of users separated by comma that you want to unlock"
-                Write-Host "${Indent}For example 'John, Andre, Matthew'"
-                $usersString = Read-Host "${Indent}" # change prompt sign to '>>' !!!
+                Write-IndentedLog "Chose option number 1 - Unlock User or Users"
+                Write-IndentedLog "Type user or list of users separated by comma that you want to unlock"
+                Write-IndentedLog "For example 'John, Andre, Matthew'"
+                # PROBLEM HERE !!! CHOSE OPTION 1 AND SOLVE IT !
+                # sprawdz czy Read-Host może mieć pusty prompt (-Prompt "") ???
+
+                $usersString = Read-IndentedLog ">>>"  # change prompt sign to '>>' !!!
+
                 $formatedList = $usersString.Split(',').Trim()
                 # pressing Enter without typing value triggers error 'ArgumentOutOfRangeException'. WHY? CHECK IT !!!
                 foreach ($user in $formatedList) {
                     try {
                         $user = Get-ADUser -Identity $user -Properties * -ErrorAction Stop
                         $username = $user.name
-                        Write-Host "${Indent}$username found in database!"
+                        Write-IndentedLog "$username found in database!"
                     } catch {
-                        Write-Host "${Indent}Account $username doesn't exist in database!"
-                        Write-Host "${Indent}Removing $username account from a list..."
+                        Write-IndentedLog "Account $username doesn't exist in database!"
+                        Write-IndentedLog "Removing $username account from a list..."
                         $formatedList.Remove($user)
                         $formatedList = $formatedList | Where-Object { $_ -ne $user }
-                        Write-Host "${Indent}$user account removed from a list"
+                        Write-IndentedLog "$user account removed from a list"
                     }   
 
-                    Write-Host "${Indent}Is $username account locked?"                        
+                    Write-IndentedLog "Is $username account locked?"                        
                     $isLocked = $user.LockedOut
                     if ($isLocked) {
-                        Write-Host "${Indent}$isLocked"
-                        Write-Host "${Indent}Account is locked"
+                        Write-IndentedLog "$isLocked"
+                        Write-IndentedLog "Account is locked"
                     } else {
-                        Write-Host "${Indent}$isLocked"
-                        Write-Host "${Indent}Cannot unlock account. Account is not locked out!"
-                        Write-Host "${Indent}Removing $username account from a list..."
+                        Write-IndentedLog "$isLocked"
+                        Write-IndentedLog "Cannot unlock account. Account is not locked out!"
+                        Write-IndentedLog "Removing $username account from a list..."
                         $formatedList = $formatedList | Where-Object { $_ -ne $username }
-                        Write-Host "${Indent}$username account removed from a list"
+                        Write-IndentedLog "$username account removed from a list"
                     }
                 }
                 
                     if ($formatedList -eq $null) {
-                        Write-Host "${Indent}### LIST ###"
-                        Write-Host "${Indent}------------"
-                        Write-Host "${Indent}List is empty. Returning to main menu"
+                        Write-IndentedLog "### LIST ###"
+                        Write-IndentedLog "------------"
+                        Write-IndentedLog "List is empty. Returning to main menu"
                     } else {
-                        Write-Host "${Indent}Accounts you wish to unlock:"
-                        Write-Host "${Indent}### LIST ###"
-                        Write-Host "${Indent}$formatedList"
+                        Write-IndentedLog "Accounts you wish to unlock:"
+                        Write-IndentedLog "### LIST ###"
+                        Write-IndentedLog "$formatedList"
                         # format LIST to look like a TABLE ( record /breakline record)
 
-                        Write-Host "${Indent}Are you sure you want to unlock these accounts?"
+                        Write-IndentedLog "Are you sure you want to unlock these accounts?"
                         while ($true) {
-                            $confirm = Read-Host "${Indent}Type [Yes/Y] or [No/N]"
+                            $confirm = Read-IndentedLog "Type [Yes/Y] or [No/N]"
                             $yesList = "y", "yes"
                             $noList = "n", "no"
 
                             if ($confirm -in $yesList) {
-                                Write-Host "${Indent}Unlocking accounts..."
+                                Write-IndentedLog "Unlocking accounts..."
                                 $formatedList | Get-ADUser | Unlock-ADAccount
-                                Write-Host "${Indent}Accounts unlocked!"
+                                Write-IndentedLog "Accounts unlocked!"
                                 break
                             } elseif ($confirm -in $noList) {
-                                Write-Host "${Indent}Accounts left locked"
+                                Write-IndentedLog "Accounts left locked"
                                 break
                             } else {
-                                Write-Host "${Indent}Invalid value!"
-                                Write-Host "${Indent}Choose one of the suggested options"
+                                Write-IndentedLog "Invalid value!"
+                                Write-IndentedLog "Choose one of the suggested options"
                             }
                         }
                     }
@@ -109,31 +131,31 @@ if ($result -eq $null) {
 
             # OPTION 2: Unlock ALL
             } elseif ($userChoice -in $allOptions) {
-                Write-Host "${Indent}Chose option number 2 - Unlock all the users"
-                Write-Host "${Indent}Unlocking all the users..."
+                Write-IndentedLog "Chose option number 2 - Unlock all the users"
+                Write-IndentedLog "Unlocking all the users..."
                 Search-ADAccount -LockedOut | Unlock-ADAccount
-                Write-Host "${Indent}All the users unlocked!"
+                Write-IndentedLog "All the users unlocked!"
                 $result = Search-ADAccount -LockedOut
             # OPTION 3: Exit
             } elseif ($userChoice -in $exitOptions) {
-                Write-Host "${Indent}Chose option number 3 - Exit"
-                Write-Host "${Indent}Terminating the program..."
+                Write-IndentedLog "Chose option number 3 - Exit" -BackgroundColor Green
+                Write-IndentedLog "Terminating the program..."
                 break
             }elseif ($userChoice -in $listOptions) {
-                Write-Host "${Indent}Chose option number 4 - Show list"
+                Write-IndentedLog "Chose option number 4 - Show list"
                 $formattedResult = $result | Format-Table Name, ObjectClass, LockedOut | Out-String
                 $indentedResult = $formattedResult -replace '(?m)^', "$Indent"
-                Write-Host "${Indent}Displaying list of locked accoutns..."
-                Write-Host "${Indent}"
-                Write-Host "${Indent}### LIST ###"
+                Write-IndentedLog "Displaying list of locked accoutns..."
+                Write-IndentedLog ""
+                Write-IndentedLog "### LIST ###"
                 $indentedResult
             } else {
-                Write-Host "${Indent}Non existent option or invalid value."
-                Write-Host "${Indent}Type number of option like 'one', '2', or 'THREE'..."
-                Write-Host "${Indent}...or option value like 'user', 'ALL' or 'Exit' "
+                Write-IndentedLog "Non existent option or invalid value."
+                Write-IndentedLog "Type number of option like 'one', '2', or 'THREE'..."
+                Write-IndentedLog "...or option value like 'user', 'ALL' or 'Exit' "
             }
         }
-        Write-Host "${Indent}Program Terminated!"
+        Write-IndentedLog "Program Terminated!"
     }
 
 ### NOTES ###
